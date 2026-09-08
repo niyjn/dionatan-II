e7e69f3643eda9530ae6748cb03f462e91cc55b6
gabster <gabri11222331@gmail.com>
2026-09-08 09:18:40 -0300

    ATV 1: Criar rotas /sobre, /alunos e /contato retornando texto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
-<?php
+﻿<?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// TEMA 1 - ATV 1: Rotas simples retornando texto
+Route::get('/sobre', function () {
+    return 'Página Sobre: Bem-vindo ao sistema!';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de Alunos: Lista de alunos matriculados.';
+});
+
+Route::get('/contato', function () {
+    return 'Página de Contato: Entre em contato conosco.';
 });
